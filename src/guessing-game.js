98dbe5132c2441f436012458b0eab67c69c4cb1e
Pavel Kachinskiy <pavel.kachinskiy@gmail.com>
2017-02-04 00:17:04 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.min = 0;
+    	this.max = 0;
+    	this.val = 0;
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+    	this.val=Math.ceil((this.max+this.min)/2);
+    	return this.val;
     }
 
     lower() {
-
+    	this.max = this.guess();
+    	
     }
 
     greater() {
-
+    	this.min = this.guess();
     }
 }
 
